8ff09ab769512e6c436f11b9e078d7cf019fbd0b
SadikAli-Karadiya <karadiyasadikali@gmail.com>
2023-05-26 11:11:52 +0530

    react-query setup completed

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ToastContainer
