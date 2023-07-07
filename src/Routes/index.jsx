5d9bcf3e5ca9b5589f913947cdb8c4cb9553e302
shadrajput <shadrajput6@gmail.com>
2023-07-07 15:31:51 +0530

    Changes route

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -11,7 +11,7 @@ function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                {true ? (
+                {token ? (
                     <>
                         <Route path='/*' element={<PrivateRoutes />} />
                     </>
